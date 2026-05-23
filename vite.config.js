afbd5a99fbc49aa5f8266afdbb14a5c0b3f51a83
SergioRC-04 <slcabana@uninorte.edu.co>
2026-05-23 07:52:51 -0500

    Arreglar vite config añadiendo un plugin.

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,42 @@
 import { defineConfig } from "vite";
+import fs from "fs";
+import path from "path";
+
+// Plugin para copiar src/components a dist/components
+const copyComponentsPlugin = () => ({
+  name: "copy-components",
+  writeBundle() {
+    const srcDir = "src/components";
+    const destDir = "dist/components";
+
+    if (!fs.existsSync(destDir)) {
+      fs.mkdirSync(destDir, { recursive: true });
+    }
+
+    const files = fs.readdirSync(srcDir);
+    files.forEach((file) => {
+      const src = path.join(srcDir, file);
+      const dest = path.join(destDir, file);
+      fs.copyFileSync(src, dest);
+    });
+  },
+});
 
 export default defineConfig({
   root: "src",
-  publicDir: "../public",
+  publicDir: false, // Desactiva publicDir
   build: {
     outDir: "../dist",
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: "src/index.html",
+        empresa: "src/empresa.html",
+        servicios: "src/servicios.html",
+      },
+    },
   },
+  plugins: [copyComponentsPlugin()],
   server: {
     port: 3000,
     open: true,
